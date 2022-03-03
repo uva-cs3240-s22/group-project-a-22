6033c31d1be2985207b0d70bc609110478e3810e
Nicholas Miller <nicholascmiller123@gmail.com>
2022-03-03 16:53:45 -0500

    Added models for recipe

--- wom/admin.py
+++ wom/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from wom.models import Ingredient, Recipe, Instruction
+
 # Register your models here.
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Instruction)
--- wom/models.py
+++ wom/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+    class MealTypes(models.TextChoices):
+        BREAKFAST = 'breakfast'
+        LUNCH = 'lunch'
+        DINNER = 'dinner'
+        DESSERT = 'dessert'
+        OTHER = 'other'
+
+    class Courses(models.TextChoices):
+        APPETIZER = 'appetizer'
+        ENTREE = 'entree'
+        SIDE = 'side'
+        SNACK = 'snack'
+        OTHER = 'other'
+
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=1000)
+    cooking_time = models.DurationField()
+    preparation_time = models.DurationField()
+    meal_type = models.CharField(
+        max_length=10, choices=MealTypes.choices, default=MealTypes.OTHER)
+    course = models.CharField(
+        max_length=10, choices=Courses.choices, default=Courses.OTHER)
+    pub_date = models.DateTimeField('Date Published')
+
+    def __str__(self):
+        return self.title
+
+
+class Ingredient(models.Model):
+    recipes = models.ManyToManyField(Recipe)
+    name = models.CharField(max_length=50)
+    quantity = models.FloatField()
+    units = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name + " (" + str(self.quantity) + " " + self.units + ")"
+
+
+class Instruction(models.Model):
+    text = models.CharField(max_length=500)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
