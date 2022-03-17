9f69a74210da1a7287fdf462f1945998f4bc4f1c
Nicholas Miller <nicholascmiller123@gmail.com>
2022-03-17 10:02:19 -0400

    Added more admin capabilities for models

--- wom/admin.py
+++ wom/admin.py
@@ -1,8 +1,39 @@
+from csv import list_dialects
 from django.contrib import admin
 
-from wom.models import Ingredient, Recipe, Instruction
+from wom.models import Ingredient, IngredientQuantity, Recipe, Instruction
 
 # Register your models here.
-admin.site.register(Recipe)
+
+
+class IngredientQuantityInline(admin.TabularInline):
+    model = IngredientQuantity
+    extra = 3
+
+
+class InstructionInline(admin.TabularInline):
+    model = Instruction
+    extra = 3
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': [
+            'title',
+            'description',
+            'preparation_time',
+            'cooking_time',
+            'meal_type',
+            'course'
+        ]}),
+        ('Date Information', {'fields': [
+         'pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [IngredientQuantityInline, InstructionInline]
+    list_display = ['title', 'pub_date', 'meal_type']
+    list_filter = ['pub_date']
+    search_fields = ['title']
+
+
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient)
-admin.site.register(Instruction)
