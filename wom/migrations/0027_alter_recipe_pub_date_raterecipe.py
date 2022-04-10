af14554f3da874a55a3589a85e4d628dcb639bd8
ald8tdd <ald8tdd@virginia.edu>
2022-04-10 15:02:48 -0400

    Merge branch 'August'

--- wom/migrations/0027_alter_recipe_pub_date_raterecipe.py
+++ wom/migrations/0027_alter_recipe_pub_date_raterecipe.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('wom', '0026_alter_recipe_pub_date'),
+        ('wom', '0025_alter_ingredient_quantity_alter_recipe_pub_date'),
     ]
 
     operations = [
