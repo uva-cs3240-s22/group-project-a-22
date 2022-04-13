856d8f445c366736be71d03cb77f064274a6b57e
Nicholas Miller <nicholascmiller123@gmail.com>
2022-04-13 15:37:43 -0400

    Wrote tests for ingredient search

--- wom/tests/test_search.py
+++ wom/tests/test_search.py
@@ -2,11 +2,11 @@
 # https://docs.djangoproject.com/en/dev/ref/models/querysets/#in
 
 from django.test import Client, TestCase
-from wom.models import Recipe
+from wom.models import Ingredient, Recipe
 from datetime import timedelta, timezone, datetime
 
 
-def create_recipe(title, description, meal_type, course, cooking_time, preparation_time):
+def create_recipe(title, description, meal_type, course, cooking_time, preparation_time, ingredients):
     """
     Create a recipe with the given parameters.
     """
@@ -16,7 +16,14 @@ def create_recipe(title, description, meal_type, course, cooking_time, preparati
     if not preparation_time:
         preparation_time = timedelta(days=20, seconds=23, microseconds=13,
                                      milliseconds=29000, minutes=27, hours=3, weeks=2)
-    return Recipe.objects.create(title=title, description=description, cooking_time=cooking_time, preparation_time=preparation_time, meal_type=meal_type, course=course)
+    recipe = Recipe.objects.create(title=title, description=description, cooking_time=cooking_time,
+                                   preparation_time=preparation_time, meal_type=meal_type, course=course)
+
+    for ingredient in ingredients:
+        Ingredient.objects.create(
+            recipe=recipe, name=ingredient, quantity=5, units='oz')
+
+    return recipe
 
 
 class SearchTests(TestCase):
@@ -27,7 +34,8 @@ class SearchTests(TestCase):
             meal_type="dinner",
             course="appetizer",
             preparation_time=timedelta(seconds=23, minutes=27, hours=3),
-            cooking_time=timedelta(seconds=23, minutes=27, hours=0)
+            cooking_time=timedelta(seconds=23, minutes=27, hours=0),
+            ingredients=['Ingredient 1', 'Ingredient 3']
         ),
         create_recipe(
             title="Demo recipe no2",
@@ -35,7 +43,8 @@ class SearchTests(TestCase):
             meal_type="breakfast",
             course="side",
             preparation_time=timedelta(seconds=23, minutes=27),
-            cooking_time=timedelta(seconds=3, minutes=20, hours=1)
+            cooking_time=timedelta(seconds=3, minutes=20, hours=1),
+            ingredients=['Ingredient 2', 'Ingredient 3']
         ),
         create_recipe(
             title="Peking Duck",
@@ -43,7 +52,8 @@ class SearchTests(TestCase):
             meal_type="lunch",
             course="dessert",
             preparation_time=timedelta(seconds=23, minutes=27, hours=1),
-            cooking_time=timedelta(seconds=53, minutes=47, hours=4)
+            cooking_time=timedelta(seconds=53, minutes=47, hours=4),
+            ingredients=['Ingredient 1', 'Ingredient 2', 'Ingredient 3']
         )
 
     def test_empty_search_input(self):
@@ -100,7 +110,8 @@ class FilterTests(TestCase):
             meal_type="dinner",
             course="appetizer",
             preparation_time=timedelta(seconds=23, minutes=27, hours=3),
-            cooking_time=timedelta(seconds=23, minutes=27, hours=0)
+            cooking_time=timedelta(seconds=23, minutes=27, hours=0),
+            ingredients=['Ingredient 1', 'Ingredient 3']
         ),
         create_recipe(
             title="Demo recipe no2",
@@ -108,7 +119,8 @@ class FilterTests(TestCase):
             meal_type="breakfast",
             course="side",
             preparation_time=timedelta(seconds=23, minutes=27),
-            cooking_time=timedelta(seconds=3, minutes=20, hours=1)
+            cooking_time=timedelta(seconds=3, minutes=20, hours=1),
+            ingredients=['Ingredient 2', 'Ingredient 3']
         ),
         create_recipe(
             title="Peking Duck",
@@ -116,7 +128,8 @@ class FilterTests(TestCase):
             meal_type="lunch",
             course="dessert",
             preparation_time=timedelta(seconds=23, minutes=27, hours=1),
-            cooking_time=timedelta(seconds=53, minutes=47, hours=4)
+            cooking_time=timedelta(seconds=53, minutes=47, hours=4),
+            ingredients=['Ingredient 1', 'Ingredient 2', 'Ingredient 3']
         )
 
     def test_filter_by_course(self):
@@ -191,3 +204,58 @@ class FilterTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(response.context['object_list'], Recipe.objects.all().filter(
             pub_date__gte=(datetime.now(tz=timezone.utc) - timedelta(days=365))).order_by('-avgRating'))
+
+    def test_filter_by_no_ingredients(self):
+        response = self.client.get('/wom/search/?ingredients=')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['object_list'], Recipe.objects.all(), ordered=False)
+
+    def test_sort_by_one_ingredient_success(self):
+        response = self.client.get('/wom/search/?ingredients=Ingredient+2')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['object_list'], Recipe.objects.filter(ingredient__name='Ingredient 2'), ordered=False)
+
+    def test_sort_by_one_ingredient_failure(self):
+        response = self.client.get('/wom/search/?ingredients=Ingredient+4')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['object_list'], Recipe.objects.none(), ordered=False)
+
+    def test_sort_by_multiple_ingredients_success(self):
+        response = self.client.get(
+            '/wom/search/?ingredients=Ingredient+2&ingredients=Ingredient+3')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['object_list'], Recipe.objects.filter(ingredient__name='Ingredient 2').filter(ingredient__name='Ingredient 3'), ordered=False)
+
+    def test_sort_by_multiple_ingredients_success(self):
+        response = self.client.get(
+            '/wom/search/?ingredients=Ingredient+4&ingredients=Ingredient+3')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['object_list'], Recipe.objects.none(), ordered=False)
+
+    def test_sort_by_ingredients_plus_others_success(self):
+        response = self.client.get(
+            '/wom/search/?meal_type=breakfast&course=side&prep_time=00%3A30%3A00&cook_time=1%3A00%3A01&ingredients=Ingredient+2')
+        filters = {'meal_type__iexact': "breakfast", 'course__iexact': 'side',
+                   'preparation_time__lte': timedelta(minutes=30), 'cooking_time__gte': timedelta(hours=1), 'ingredient__name': 'Ingredient 2'}
+
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['object_list'], Recipe.objects.filter(**filters), ordered=False)
+
+    def test_sort_by_ingredients_plus_others_failure(self):
+        response = self.client.get(
+            '/wom/search/?meal_type=breakfast&course=side&prep_time=00%3A30%3A00&cook_time=1%3A00%3A01&ingredients=Ingredient+1')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['object_list'], Recipe.objects.none(), ordered=False)
